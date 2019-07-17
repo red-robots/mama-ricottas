39840d8e9edc558a379aedc21fe48c6d5aec291f
Lisa <annalisa.quiamco@gmail.com>
2019-07-16 19:48:15 -0600

    Styling Contact Us page

--- page-contact.php
+++ page-contact.php
@@ -1,22 +1,13 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package ACStarter
+ * Template Name: Contact
  */
 
 get_header(); 
 $banner = get_field("banner");
 ?>
 
-	<div id="primary" class="full-content-area clear default">
+	<div id="primary" class="full-content-area clear default contact-page">
 		<main id="main" class="site-main clear" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
