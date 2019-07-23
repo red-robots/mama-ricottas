58ed2237991a54cfd7ad2e56991925632726c6f3
Lisa <annalisa.quiamco@gmail.com>
2019-07-22 20:57:37 -0600

    added video theme_support

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -18,7 +18,10 @@ function acstarter_setup() {
 
   // Add default posts and comments RSS feed links to head.
   add_theme_support( 'automatic-feed-links' );
-
+  add_theme_support( 'custom-header', array(
+      'video' => true
+  ) );
+  
   /*
    * Let WordPress manage the document title.
    * By adding theme support, we declare that this theme does not use a
