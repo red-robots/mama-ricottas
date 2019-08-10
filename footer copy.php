e75425ed6458342cffeb6edd8d1510bffc173ca2
Lisa <annalisa.quiamco@gmail.com>
2019-08-09 19:39:49 -0600

    Added contact info at the footer section

--- footer copy.php
+++ footer copy.php
@@ -19,10 +19,6 @@
 
 		$footer_text = get_field('partners_text','option');
 		$partners = get_field('partners_list','option');
-
-		$address = get_field('address','option');
-		$phone = get_field('phone','option');
-		$hours = get_field('hours','option');
 	?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php if ($reservation_title && $reservation_link) { ?>
@@ -35,41 +31,18 @@
 
 		<div class="footbottom clear">
 			<div class="wrapper">
-
-				<div class="foot-contact-info">
-					<div class="midwrap">
-						<?php if ($social_links) { ?>
-						<div class="social-media-col col3 clear">
-							<div class="vertical-middle">
-								<span class="sp span1">FOLLOW US</span>
-								<span class="sp span2">
-									<?php foreach ($social_links as $k=>$e) { ?>
-										<a class="<?php echo $k ?>" href="<?php echo $e[0] ?>" target="_blank"><i class="<?php echo $e[1] ?>"></i><span class="sr-only"><?php echo $k ?></span></a>
-									<?php } ?>
-								</span>
-							</div>
-						</div>	
-						<?php } ?>
-
-						<?php if ($address || $phone) { ?>
-						<div class="foot-address col3">
-							<address>
-								<?php echo $address; ?>
-								<?php if ($phone) { ?>
-								<div class="phone"><a href="<?php echo format_phone_number($phone); ?>"><?php echo $phone ?></a></div>
-								<?php } ?>
-							</address>
-						</div>
-						<?php } ?>
-
-						<?php if ($hours) { ?>
-						<div class="foot-hours col3">
-							<?php echo $hours; ?>
-						</div>
-						<?php } ?>
+				<?php if ($social_links) { ?>
+				<div class="social-media clear">
+					<div class="vertical-middle">
+						<span class="sp span1">FOLLOW US</span>
+						<span class="sp span2">
+							<?php foreach ($social_links as $k=>$e) { ?>
+								<a class="<?php echo $k ?>" href="<?php echo $e[0] ?>" target="_blank"><i class="<?php echo $e[1] ?>"></i><span class="sr-only"><?php echo $k ?></span></a>
+							<?php } ?>
+						</span>
 					</div>
-				</div>
-				
+				</div>	
+				<?php } ?>
 
 				<?php if ($footer_text) { ?>
 				<div class="footer-text text-center clear"><?php echo $footer_text ?></div>
